ae5327341e2e37c0a90dbb44bdbd154510dd3d22
Jose Luis Lopez Michme <j.lopezmichme@itsrizzoli.it>
2023-12-13 16:29:32 +0100

    Conessione databases

--- README.md
+++ README.md
@@ -4,6 +4,11 @@
 
 conda create -n redis python=3.11
 
+-username, Password
+
+    user = progetto_redis
+    pass = b7^WJ#!4
+
 - Librerie installate : 
 
 pip install redis
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-print("ciao mondo")
+#print("ciao mondo")
 
 import redis
 
-r = redis.Redis(host='', port='', decode_responses='')
+
+r = redis.Redis(
+  host='redis-11706.c281.us-east-1-2.ec2.cloud.redislabs.com',
+  port=11706,
+  password='01wNM4dZYmFHBCfiHGGUzLOFpo69MTxk')
 
