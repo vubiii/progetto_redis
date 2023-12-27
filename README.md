40538951fc014ab57604f7f9919e93c30b451ea9
Jose Luis Lopez Michme <j.lopezmichme@itsrizzoli.it>
2023-12-27 14:11:42 +0100

    Aggiunta una interfaccia utente

--- README.md
+++ README.md
@@ -12,3 +12,14 @@
     user = default
     pass = 01wNM4dZYmFHBCfiHGGUzLOFpo69MTxk
     port = 11706
+
+    vecchio db : 
+    
+    host='redis-11706.c281.us-east-1-2.ec2.cloud.redislabs.com',
+    port=11706,
+    password='01wNM4dZYmFHBCfiHGGUzLOFpo69MTxk'
+
+    nuovo database : 
+    host : redis-10048.c304.europe-west1-2.gce.cloud.redislabs.com
+    port : 10048
+    pass : 1x5x6xi9x0SGA4uOuErpndO5H8xYH9dG
--- main.py
+++ main.py
@@ -14,7 +14,8 @@ class RedisManager:
                 host=self.host,
                 port=self.port,
                 password=self.password,
-                decode_responses=True  
+                decode_responses=True,
+                encoding='utf-8'  
             )
             print("Connessione riuscita")
         except Exception as e:
@@ -34,10 +35,15 @@ class RedisManager:
       while True:
           username_input = input("Inserire il nome utente: ")
           password_input = input("Inserire password: ")
-          u = self.connection.get(f"username:{username_input}")  
-          if u is None:
+
+          u = self.connection.hget('utenti:1', 'username')  
+          print(f'valore ritornato di u: {u}')  
+          # se l'utente non esiste oppure se il contenuto della chiave e diversa dal input si può registrare
+          if u is None or u != username_input: 
               try:
-                  self.connection.set(f"username:{username_input}", f"password:{password_input}")
+                  self.connection.hset('utenti:1', mapping={'username': f'{username_input}'
+                                                            , 'password':f'{password_input}'
+                                                            , 'user_ID':f'{1}'})
                   print(f"Registrazione utente completato, benvenuto {username_input}")
                   break 
               except Exception as e:
@@ -50,27 +56,78 @@ class RedisManager:
                   break  
 
     def login(self, username, password):
-        print("Login Utente")
-        stored_password = self.connection.get(f"username:{username}")
-        if stored_password == f"password:{password}":
+        print("-----. Login Utente .-----")
+
+        stored_utente = self.connection.hgetall(name='utenti:1')
+
+        if stored_utente['username'] == username and stored_utente['password'] == password:
             print(f"Accesso effettuato con successo, benvenuto {username}")
+            return True
         else:
             print("Credenziali errate. Riprova.")
+            return False
 
-
+    #temporaneo
+    def get_connection(self):
+        return self.connection
   
 
 if __name__ == "__main__":
+
     redis_manager = RedisManager(
-        host='redis-11706.c281.us-east-1-2.ec2.cloud.redislabs.com',
-        port=11706,
-        password='01wNM4dZYmFHBCfiHGGUzLOFpo69MTxk'
+        host='redis-10048.c304.europe-west1-2.gce.cloud.redislabs.com',
+        port= 10048,
+        password='1x5x6xi9x0SGA4uOuErpndO5H8xYH9dG'
     )
-    print("Redis Chat")
+    print("----.Redis Chat.----")
     print("")
-    redis_manager.open()
     
+    #redis_manager.open()
+    
+    #Interfaccia Iniziale
+    while True:
+        print('\n----- Opzioni disponibili -----'
+              '\n   1 - Login'
+              '\n   2 - Registrazione'
+              '\n   0 - Uscire'
+              '\n----- Opzioni disponibili -----')
 
-    redis_manager.close()
+        choice = input('Inserisci un numero: ')
+        
+        redis_manager.open()
 
+        #Conessione per l'inserimento-recupero dati:
+        connection = redis_manager.get_connection()
 
+        match choice:
+            case '1':
+                user = input('Inserisic il nome utente :') 
+                password = input('Inserisice la password :')
+
+                if redis_manager.login(user, password):
+                    while True:
+                        print('\n----Interfaccia Utente Loggato-----:'
+                              '\n   1 - Contatti'
+                              '\n   2 - Modalità'
+                              '\n   3 - Logout'
+                              '\n------------------------------------')
+                        
+                        user_choice = input('Inserisci un numero: ')
+                        
+                        match user_choice:
+                            case '1':
+                                print('Visualizza lista contatti')
+                                # Implementa la logica per la gestione dei contatti
+                            case '2':
+                                print('Seleziona modalità')
+                                # Implementa la logica per la selezione della modalità
+                            case '3':
+                                print('Logout effettuato')
+                                redis_manager.close()
+                                break
+            case '2':
+                redis_manager.register()
+                #print(connection.hgetall('utenti:1')) # -> ritorna un dizionario
+                print('hgetall di utenti:1 -> ', connection.hgetall('utenti:1')) 
+            case '0':
+                break
